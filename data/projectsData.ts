d310407a665db4796ef74ef3766b6fc2575db896
Sebastian Korfmann <sebastian@korfmann.net>
2022-05-17 22:54:53 +0200

    Update content

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,15 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'CDK for Terraform',
+    description: `Cloud Development Kit for Terraform allows you to use familiar programming languages to define cloud infrastructure and provision it through HashiCorp Terraform. This gives you access to the entire Terraform ecosystem without learning HashiCorp Configuration Language (HCL) and lets you leverage the power of your existing toolchain for testing, dependency management and more`,
+    imgSrc: '/static/images/cdktf.png',
+    href: 'https://cdk.tf',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'cdk.dev',
+    description: `Co-Founded cdk.dev with other people from the broader CDK community. cdk.dev is a Slack community for sharing and discussing CDK resources and best practices with more than 4000 members.`,
+    imgSrc: '/static/images/cdk-dev.jpeg',
+    href: 'https://cdk.dev',
   },
 ]
 
